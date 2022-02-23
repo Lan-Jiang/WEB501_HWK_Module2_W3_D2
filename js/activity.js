f5248cc5cdd48a3b5484df44262dcca578614ead
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-02-22 23:21:35 -0800

    WEB501 HWK M2 W3 D2 finished

--- js/activity.js
+++ js/activity.js
@@ -15,24 +15,53 @@ $(document).ready(function() {
     // });
 
     //click function and show info box
-    $("td").click(function() {
+    // $("td").click(function() {
+    //     var content = $(this).text();
+    //     var thIndex = $(this).index();
+    //     var thContent = $("table").find("th").eq(thIndex).text()
+
+    //     if (content != "Not Available") {
+    //         $(this).toggleClass("selected");
+
+    //         if ($(this).hasClass("selected")) {
+    //             $("#displaySelected").css("visibility", "visible");
+    //             $("#displaySelected").css("margin-top", "2em");
+    //             $("#result").append("<p>" + content + " at " + thContent + "</p>"); 
+    //             $("#GFGresult").append("<p>" + content + " at " + thContent + "</p>"); 
+
+    //         } else {
+    //             $("#result p:contains(" + content + ")").remove();
+    //             $("#GFGresult p:contains(" + content + ")").remove();
+
+    //             if ($("#result").has("p").length == false) {
+    //                 $("#displaySelected").css("visibility", "hidden");
+    //                 $("#displaySelected").css("margin-top", "0");
+    //             }
+
+    //             if ($("#GFGresult").has("p").length == false) {
+    //                 $("#gfgmodal").modal('hide');
+    //             }
+    //         }
+    //     }
+    // });
+
+    //click function and show info box
+    $("td").not("table tbody tr td:first-child").click(function() {
         var content = $(this).text();
         var thIndex = $(this).index();
         var thContent = $("table").find("th").eq(thIndex).text()
-
+    
         if (content != "Not Available") {
             $(this).toggleClass("selected");
-
+    
             if ($(this).hasClass("selected")) {
-                $("#displaySelected").css("visibility", "visible");
-                $("#displaySelected").css("margin-top", "2em");
-                $("#result").append("<p>" + content + " at " + thContent + "</p>");             
+                $("#ActivitiesResult").append("<p>" + content + " at " + thContent + "</p>"); 
+    
             } else {
-                $("#result p:contains(" + content + ")").remove();
-
-                if ($("#result").has("p").length == false) {
-                    $("#displaySelected").css("visibility", "hidden");
-                    $("#displaySelected").css("margin-top", "0");
+                $("#ActivitiesResult p:contains(" + content + ")").remove();
+    
+                if ($("#ActivitiesResult").has("p").length == false) {
+                    $("#activitiesmodal").modal('hide');
                 }
             }
         }
